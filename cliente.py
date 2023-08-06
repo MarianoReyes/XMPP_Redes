630f0989e63b5ef6a60e23831a3a309a2bd75d69
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-08-06 09:23:07 -0600

    trabajos en cliente

--- cliente.py
+++ cliente.py
@@ -1,4 +1,15 @@
 import xmpp
+import slixmpp
+from slixmpp.exceptions import IqError, IqTimeout
+from slixmpp.xmlstream.stanzabase import ET
+from aioconsole import ainput
+from aioconsole.stream import aprint
+import asyncio
+from asyncio import Future
+from typing import Optional, Union
+import tkinter as tk
+from tkinter import messagebox
+import menus
 
 
 # implementacion modifica de registro simple extraido de repositorio https://github.com/xmpppy/xmpppy
@@ -12,3 +23,28 @@ def register(client, password):
             'username': jid.getNode(),
             'password': password
         }))
+
+
+class Cliente(slixmpp.ClientXMPP):
+    def __init__(self, jid, password):
+
+        super().__init__(jid, password)
+        self.name = jid.split('@')[0]
+        self.is_connected = False
+        self.actual_chat = ''
+
+        # generado por IA
+        self.register_plugin('xep_0030')  # Service Discovery
+        self.register_plugin('xep_0199')  # Ping
+        self.register_plugin('xep_0045')  # MUC
+        self.register_plugin('xep_0085')  # Notifications
+        self.register_plugin('xep_0004')  # Data Forms
+        self.register_plugin('xep_0060')  # PubSub
+
+        # eventos
+        self.add_event_handler('session_start', self.start)
+        self.add_event_handler('subscription_request',
+                               self.accept_subscription)
+        self.add_event_handler('message', self.chat_received)
+        self.add_event_handler('disco_items', self.print_rooms)
+        self.add_event_handler('groupchat_message', self.chatroom_message)
